c71a7f6e03213561682f7f514805988bac419378
hspadim <henrique@spadim.com.br>
2022-02-27 20:28:32 -0300

    Add part 2 of day 4

--- src/day4.rs
+++ src/day4.rs
@@ -4,6 +4,16 @@ use itertools::Itertools;
 
 const _DUMMY_INPUT: &str = include_str!("data/day4-dummy.txt");
 const REAL_INPUT: &str = include_str!("data/day4-real.txt");
+const PART_2_MUST_FIND_THIS: &str = "northpoleobjectstorage";
+
+fn decrypt(name: &str, id: u32) -> String {
+    name.chars()
+        .map(|c| {
+            ((((c as u32 - 'a' as u32) + id) % ('z' as u32 - 'a' as u32 + 1)) + 'a' as u32) as u8
+                as char
+        })
+        .collect()
+}
 
 fn private_solve_part_1(values: &str) -> String {
     values
@@ -51,8 +61,45 @@ fn private_solve_part_1(values: &str) -> String {
         .to_string()
 }
 
-fn private_solve_part_2(_values: &str) -> String {
-    unimplemented!()
+fn private_solve_part_2(values: &str) -> String {
+    for line in values.lines() {
+        let mut counter: HashMap<char, i32> = HashMap::new();
+        let mut room = line.chars().filter(|&c| c != '-').peekable();
+        let encrypted_name = room
+            .by_ref()
+            .peeking_take_while(|&c| c.is_ascii_alphabetic())
+            .collect::<String>();
+        let sector_id = room
+            .by_ref()
+            // .skip_while(|&c| c.is_ascii_alphabetic())
+            .take_while(|&c| c != '[')
+            .collect::<String>()
+            .parse::<u32>()
+            .unwrap();
+        let checksum = room
+            .by_ref()
+            // .skip_while(|&c| c != '[')
+            .take_while(|&c| c != ']')
+            .collect::<String>();
+        for c in encrypted_name.chars() {
+            *counter.entry(c).or_insert(0) += 1;
+        }
+        let mut hash_vec = counter.iter().collect::<Vec<(&char, &i32)>>();
+        hash_vec.sort_unstable_by(|&a, &b| match b.1.cmp(a.1) {
+            std::cmp::Ordering::Less => std::cmp::Ordering::Less,
+            std::cmp::Ordering::Equal => a.0.cmp(b.0),
+            std::cmp::Ordering::Greater => std::cmp::Ordering::Greater,
+        });
+        let response = hash_vec
+            .iter()
+            .map(|(&a, _b)| a)
+            .take(5)
+            .collect::<String>();
+        if checksum == response && decrypt(&encrypted_name, sector_id) == PART_2_MUST_FIND_THIS {
+            return sector_id.to_string();
+        }
+    }
+    panic!("Sector id not found!")
 }
 
 fn _solve_part_1_dummy() -> String {
@@ -80,8 +127,9 @@ mod tests {
         assert_eq!((123 + 987 + 404).to_string(), _solve_part_1_dummy());
     }
     #[test]
+    #[should_panic]
     fn test_part_2_dummy() {
-        assert_eq!("", _solve_part_2_dummy());
+        _solve_part_2_dummy();
     }
     #[test]
     fn test_part_1_real() {
