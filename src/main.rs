ec5b69a19adfe28245ac19957108da88fd77e6bc
hspadim <henrique@spadim.com.br>
2022-02-27 21:26:47 -0300

    Add day5 setup

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,8 @@ fn main() -> anyhow::Result<()> {
         "3-2" => println!("{}", p3_2()),
         "4-1" => println!("{}", p4_1()),
         "4-2" => println!("{}", p4_2()),
+        "5-1" => println!("{}", p5_1()),
+        "5-2" => println!("{}", p5_2()),
         _ => return Err(anyhow!("Unknown puzzle")),
     }
     Ok(())
