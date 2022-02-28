f9f05d380326050c9ff1bb8edfd9cb7ae1b42bae
hspadim <henrique@spadim.com.br>
2022-02-27 22:04:21 -0300

    Finish first part of day 5

--- src/day5.rs
+++ src/day5.rs
@@ -1,8 +1,21 @@
 const _DUMMY_INPUT: &str = include_str!("data/day5-dummy.txt");
 const REAL_INPUT: &str = include_str!("data/day5-real.txt");
+const PASSWORD_LENGTH: usize = 8;
+const HASH_MATCH: &str = "00000";
 
 fn private_solve_part_1(values: &str) -> String {
-    unimplemented!()
+    let mut output = String::new();
+    let mut index = 0;
+    while output.len() < PASSWORD_LENGTH {
+        let data = values.to_string() + &index.to_string();
+        let hash = format!("{:x}", md5::compute(data.as_bytes()));
+        if let Some(suffix) = hash.strip_prefix(HASH_MATCH) {
+            output.push(suffix.chars().next().unwrap());
+        }
+        index += 1;
+    }
+
+    output
 }
 
 fn private_solve_part_2(values: &str) -> String {
@@ -31,7 +44,7 @@ mod tests {
 
     #[test]
     fn test_part_1_dummy() {
-        assert_eq!("", _solve_part_1_dummy());
+        assert_eq!("18f47a30", _solve_part_1_dummy());
     }
     #[test]
     fn test_part_2_dummy() {
